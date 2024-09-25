104de2b82093352ef82d598a10f0939088a5fb9d
Hoan Tran <xhoantran@gmail.com>
2024-09-25 10:21:31 -0400

    Update sqs policy

--- image-processing-pipeline/sqs.tf
+++ image-processing-pipeline/sqs.tf
@@ -1,22 +1,23 @@
+data "aws_iam_policy_document" "s3_event_notification_policy" {
+  statement {
+    actions   = ["sqs:SendMessage"]
+    resources = ["${aws_sqs_queue.queue.arn}"]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = ["${aws_s3_bucket.bucket.arn}"]
+    }
+  }
+}
+
 # SQS queue
 resource "aws_sqs_queue" "queue" {
   name           = "${var.app_prefix}-s3-event-notification"
-  policy         = <<POLICY
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Principal": "*",
-        "Action": "sqs:SendMessage",
-        "Resource": "arn:aws:sqs:*:*:${var.app_prefix}-s3-event-notification-queue",
-        "Condition": {
-          "ArnEquals": { "aws:SourceArn": "${aws_s3_bucket.bucket.arn}" }
-        }
-      }
-    ]
-  }
-  POLICY
+  policy         = data.aws_iam_policy_document.s3_event_notification_policy.json
   redrive_policy = <<POLICY
   {
     "deadLetterTargetArn": "${aws_sqs_queue.dead_letter_queue.arn}",
