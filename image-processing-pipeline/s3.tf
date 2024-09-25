d467e5a9d8e09c3323134e616135e0ade4496761
Hoan Tran <xhoantran@gmail.com>
2024-09-25 10:15:30 -0400

    Add S3 trigger and event source mapping

--- image-processing-pipeline/s3.tf
+++ image-processing-pipeline/s3.tf
@@ -4,18 +4,18 @@ resource "aws_s3_bucket" "bucket" {
 }
 
 # S3 event filter
-# resource "aws_s3_bucket_notification" "bucket_notification" {
-#   bucket = aws_s3_bucket.bucket.id
-#   queue {
-#     queue_arn = aws_sqs_queue.queue.arn
-#     events    = ["s3:ObjectCreated:*"]
-#   }
-# }
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.bucket.id
+  queue {
+    queue_arn = aws_sqs_queue.queue.arn
+    events    = ["s3:ObjectCreated:*"]
+  }
+}
 
-# # Event source from SQS
-# resource "aws_lambda_event_source_mapping" "event_source_mapping" {
-#   event_source_arn = aws_sqs_queue.queue.arn
-#   enabled          = true
-#   function_name    = aws_lambda_function.sqs_processor.arn
-#   batch_size       = 1
-# }
+# Event source from SQS
+resource "aws_lambda_event_source_mapping" "event_source_mapping" {
+  event_source_arn = aws_sqs_queue.queue.arn
+  enabled          = true
+  function_name    = aws_lambda_function.sqs_processor.arn
+  batch_size       = 1
+}
